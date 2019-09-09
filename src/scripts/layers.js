d9a507feedb43ab68e9dabb6e04f9bb2d335a29b
kjacobsen16 <kjacobsen@contractor.usgs.gov>
2019-09-09 16:03:46 -0500

    issue #37, fix Land Use layer

--- src/scripts/layers.js
+++ src/scripts/layers.js
@@ -164,7 +164,7 @@ require([
                         includeLegend: false
                     }
                 },
-                "Land Use 2012 (NLCD)": {
+                "NLCD Land Use 2012": {
                     url: "https://gis.wim.usgs.gov/arcgis/rest/services/SWTrends/lu2012_100515_test/ImageServer",
                     options: {
                         id: "lu2012",
